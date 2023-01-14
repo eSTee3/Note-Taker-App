77efc88320cf51735ccf6a532baa4e2db40a51b3
eSTee3 <tagsxa@yahoo.com>
2023-01-13 18:39:55 -0600

    Added lots o' code to several files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,15 @@
+// Adds requirements for pat and express.js
+const path = require('path');
+const router = require('express').Router();
 
+//This will send any added/saved notes, to the HTML file, for display to the end user
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+// Send user back to the home page if there's an issue
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports = router;
